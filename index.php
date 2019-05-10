93d26b71a76f9c2f6b0ffe7a869395e703150bf8
Syafrin <syafrinibrahim12@gmail.com>
2019-05-10 07:58:55 -0700

    update c

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
 <!-- Latest compiled and minified JavaScript -->
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
     
-    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
    
 </head>
 <body>
